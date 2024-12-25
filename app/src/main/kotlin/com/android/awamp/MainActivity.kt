48cb35d995bdad4bea6e0a18e9d9c0f3bd7f0708
Игорь Сергеев <igorsergeev@MacBook-Pro-Igor.local>
2024-12-26 02:25:07 +0300

    Implemented dark theme

--- app/src/main/kotlin/com/android/awamp/MainActivity.kt
+++ app/src/main/kotlin/com/android/awamp/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        setTheme(R.style.Base_Theme_Komaric)
+
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
